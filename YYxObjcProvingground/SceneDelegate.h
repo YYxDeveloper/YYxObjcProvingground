ba2a9ee413c668cdf0ef29357ad73b9a3e9f8063
qw <>
2021-09-04 22:05:53 +0800

    //

---
+++ YYxObjcProvingground/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  YYxObjcProvingground
+//
+//  Created by qw on 2021/9/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ YYxObjcProvingground/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  YYxObjcProvingground
+//
+//  Created by qw on 2021/9/4.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
